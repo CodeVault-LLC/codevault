1b1d8bec4a59e9d21ac5a8cbeed2a91977dbdc9b
lukasolsen <lukmarwil@gmail.com>
2024-04-12 22:53:47 +0200

    Update Tailwind configuration and Navbar component

--- app/layout/layout.tsx
+++ app/layout/layout.tsx
@@ -1,13 +1,27 @@
-import { Outlet } from "@remix-run/react";
+import { Outlet, useLocation } from "@remix-run/react";
 import Footer from "~/components/Footer";
 import Navbar from "~/components/Navbar";
 
 export default function Layout() {
+  const location = useLocation();
+  const isHomepage = location.pathname === "/";
+
   return (
-    <div className="dark:bg-dark-theme dark:text-white min-h-screen">
-      <Navbar />
-      <Outlet />
-      <Footer />
+    <div className="dark:bg-zinc-950 dark:text-white text-black min-h-screen grid grid-cols-1 md:grid-cols-layout">
+      {/* Sidebar (conditionally rendered) */}
+      {!isHomepage && (
+        <div className="md:col-span-1">
+          <h1>Hello World</h1>
+        </div>
+      )}
+      {/* Main content */}
+      <div className="md:col-span-2 flex flex-col">
+        <Navbar />
+        <div className="flex flex-1">
+          <Outlet />
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,22 @@
+import clsx from "clsx";
+import {
+  PreventFlashOnWrongTheme,
+  ThemeProvider,
+  useTheme,
+} from "remix-themes";
+import { themeSessionResolver } from "~/session.server";
+
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import {
   Links,
   LiveReload,
   Meta,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
-import stylesheet from "~/tailwind.css";
+import stylesheet from "~/globals.css";
 import "./styles/prismThemeDark.css";
 import Layout from "./layout/layout";
 
@@ -16,13 +25,33 @@ export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
 ];
 
-export default function App() {
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { getTheme } = await themeSessionResolver(request);
+  return {
+    theme: getTheme(),
+  };
+}
+
+export default function AppWithProviders() {
+  const data = useLoaderData<typeof loader>();
   return (
-    <html lang="en">
+    <ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
+      <App />
+    </ThemeProvider>
+  );
+}
+
+export function App() {
+  const data = useLoaderData<typeof loader>();
+  const [theme] = useTheme();
+
+  return (
+    <html lang="en" className={clsx(theme)}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
         <Links />
       </head>
       <body>
